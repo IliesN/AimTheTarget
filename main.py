e281ec8a93516c0f3fdc7bc0a30a45f675798f78
ismardn <ismael.radouane@efrei.net>
2024-02-14 22:30:04 +0100

    modifications accueil

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 
 pygame.init()
 
-LARGEUR_FENETRE, HAUTEUR_FENETRE = 1280, 720
+LARGEUR_FENETRE, HAUTEUR_FENETRE = 1280, 500
 
 
 # Import de toutes les images
@@ -19,13 +19,14 @@ image_monstre_1 = pygame.image.load("elements_decor/monstre_1.png")
 image_decor_accueil = pygame.image.load("decors/decor_accueil.jpg")
 
 image_effet_bouton = pygame.image.load("effet_bouton.png")
-DIMENSION_EFFET_BOUTON = 225
+DIMENSION_EFFET_BOUTON = 175
 
 image_bouton_jouer = pygame.image.load("bouton_jouer.png")
 bouton_jouer_rect = image_bouton_jouer.get_rect()
-DIMENSION_BOUTON = 200
+DIMENSION_BOUTON = 150
+PLACEMENT_HAUTEUR_SUPP_BT = 35
 bouton_jouer_rect.x = LARGEUR_FENETRE / 2 - DIMENSION_BOUTON / 2
-bouton_jouer_rect.y = HAUTEUR_FENETRE / 2 + 50
+bouton_jouer_rect.y = HAUTEUR_FENETRE / 2 + PLACEMENT_HAUTEUR_SUPP_BT
 
 # image_boulet_canon = pygame.image.load("decors/boulet_canon.png")
 # image_roue_canon = pygame.image.load("decors/roue_canon.png")
@@ -62,10 +63,11 @@ while en_execution:
         fenetre.blit(image_bouton_jouer, bouton_jouer_rect)
 
         if LARGEUR_FENETRE / 2 - DIMENSION_EFFET_BOUTON / 2 < souris_x < LARGEUR_FENETRE / 2 - DIMENSION_EFFET_BOUTON /\
-                2 + DIMENSION_EFFET_BOUTON and HAUTEUR_FENETRE / 2 + 50 < souris_y < HAUTEUR_FENETRE / 2 + 50 +\
-                DIMENSION_EFFET_BOUTON:
+                2 + DIMENSION_EFFET_BOUTON and HAUTEUR_FENETRE / 2 + PLACEMENT_HAUTEUR_SUPP_BT < souris_y <\
+                HAUTEUR_FENETRE / 2 + PLACEMENT_HAUTEUR_SUPP_BT + DIMENSION_EFFET_BOUTON:
             fenetre.blit(image_effet_bouton,
-                         (LARGEUR_FENETRE / 2 - DIMENSION_EFFET_BOUTON / 2, HAUTEUR_FENETRE / 2 + 35))
+                         (LARGEUR_FENETRE / 2 - DIMENSION_EFFET_BOUTON / 2, HAUTEUR_FENETRE / 2 +
+                          PLACEMENT_HAUTEUR_SUPP_BT - 10))
         else:
             fenetre.blit(image_effet_bouton, (LARGEUR_FENETRE / 2 - DIMENSION_EFFET_BOUTON / 2, HAUTEUR_FENETRE))
 
